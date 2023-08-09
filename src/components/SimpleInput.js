b9b5a7b2f26e8c14998cf1e514505c30dda998c4
iAmMuadh <adeyemimuaz1@gmail.com>
2023-08-09 02:15:24 +0100

    added validation to form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,25 +2,29 @@ import { useState } from "react";
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const nameChangeHandler = (e) => {
     setEnteredName(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if ( enteredName.trim().length === 0 ) {
+    if (enteredName.trim().length === 0) {
+      setIsValid(false);
       return;
     }
 
-    setEnteredName('')
-    console.log(enteredName)
+    setIsValid(true);
+    setEnteredName("");
+    console.log(enteredName);
+  };
 
-  }
+  const formClass = !isValid ? "form-control invalid" : "form-control";
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={formClass}>
         <label htmlFor="name">Your Name</label>
         <input
           type="text"
@@ -28,6 +32,7 @@ const SimpleInput = () => {
           onChange={nameChangeHandler}
           value={enteredName}
         />
+        {!isValid && <p className="error-text">Name cannot be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
